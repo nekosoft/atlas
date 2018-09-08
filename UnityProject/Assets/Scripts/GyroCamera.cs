d225c014cb7c1062690384684e95cf0f9ca73756
Rei Ito <Rei.Ito99@gmail.com>
2018-09-08 16:50:17 +1000

    camera on phone with cube in background

---
+++ UnityProject/Assets/Scripts/GyroCamera.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroCamera : MonoBehaviour
+{
+
+    private Gyroscope gyro;
+    private bool gyroSupported;
+    private Quaternion rotFix;
+
+    [SerializeField]
+    private Transform worldObj;
+    private float startY;
+
+
+    void Start()
+    {
+        gyroSupported = SystemInfo.supportsGyroscope;
+
+        GameObject camParent = new GameObject("camParent");
+        camParent.transform.position = transform.position;
+        transform.parent = camParent.transform;
+
+        if (gyroSupported)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            camParent.transform.rotation = Quaternion.Euler(90f, 180f, 0f);
+            rotFix = new Quaternion(0, 0, 1, 0);
+        }
+    }
+
+    private void Update()
+    {
+        if (gyroSupported && startY == 0)
+        {
+            ResetGyro();
+        }
+
+        transform.rotation = gyro.attitude * rotFix;
+    }
+
+    void ResetGyro()
+    {
+        startY = transform.eulerAngles.y;
+        worldObj.rotation = Quaternion.Euler(0, startY, 0);
+    }
+}
